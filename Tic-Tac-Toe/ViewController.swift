49692b3a77c2f0b90ffc84074bd890495676e1dd
Dylan Rothfeld <drothfel@gmail.com>
2017-09-25 11:45:18 -0700

    color change logic working

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     // Game Values
     var gameState = [[Int]]()
     var playerTurn = "RED"
-
+    
+    // UI Elements
+    @IBOutlet weak var B00: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let gestureRecognizerOne = UITapGestureRecognizer(target: self, action: #selector(tap))
+        B00.addGestureRecognizer(gestureRecognizerOne)
+        B00.isUserInteractionEnabled = true
+        gestureRecognizerOne.numberOfTapsRequired = 1
+        gestureRecognizerOne.numberOfTouchesRequired = 1
+    }
+    
+    // User Touch Interatction
+    @objc func tap(gestureRecognizer: UITapGestureRecognizer) {
+        self.B00.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
