0fd6bd8d53608a8aa27057d6abab9bae7e123e10
Dylan Rothfeld <drothfeld@eduroam-169-233-197-3.ucsc.edu>
2017-09-25 09:07:14 -0700

    init

---
+++ Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Dylan Rothfeld on 9/25/17.
+//  Copyright © 2017 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
