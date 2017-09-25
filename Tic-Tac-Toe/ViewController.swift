5bfa892ca6a557ac183ef15cae5acb617b50e472
Dylan Rothfeld <drothfel@gmail.com>
2017-09-25 12:03:28 -0700

    tap response logic

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -10,25 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // UI Elements
+    @IBOutlet weak var B00: UIImageView!
+    @IBOutlet weak var B01: UIImageView!
+    @IBOutlet weak var B02: UIImageView!
+    @IBOutlet weak var B03: UIImageView!
+    @IBOutlet weak var B04: UIImageView!
+    @IBOutlet weak var B05: UIImageView!
+    @IBOutlet weak var B06: UIImageView!
+    @IBOutlet weak var B07: UIImageView!
+    @IBOutlet weak var B08: UIImageView!
+    
     // Game Values
     var gameState = [[Int]]()
     var playerTurn = "RED"
     
-    // UI Elements
-    @IBOutlet weak var B00: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let gestureRecognizerOne = UITapGestureRecognizer(target: self, action: #selector(tap))
-        B00.addGestureRecognizer(gestureRecognizerOne)
-        B00.isUserInteractionEnabled = true
-        gestureRecognizerOne.numberOfTapsRequired = 1
-        gestureRecognizerOne.numberOfTouchesRequired = 1
+        gameSetup()
+    }
+    
+    func gameSetup() {
+        let gameImageViews = [B00, B01, B02, B03, B04, B05, B06, B07, B08]
+        
+        for imageView in gameImageViews {
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapResponse))
+            imageView?.addGestureRecognizer(tapGestureRecognizer)
+            imageView?.isUserInteractionEnabled = true
+            tapGestureRecognizer.numberOfTapsRequired = 1
+            tapGestureRecognizer.numberOfTouchesRequired = 1
+        }
     }
     
     // User Touch Interatction
-    @objc func tap(gestureRecognizer: UITapGestureRecognizer) {
+    @objc func tapResponse(gestureRecognizer: UITapGestureRecognizer) {
+        let tappedImageColor = gestureRecognizer.view?.backgroundColor
         self.B00.backgroundColor = UIColor.red
     }
 
